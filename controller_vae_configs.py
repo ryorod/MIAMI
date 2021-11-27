805fe55716e20cf7caab17f8415b1a68dbe58487
ryorod <ryo3ishi28@gmail.com>
2021-11-27 09:12:56 +0900

    wip

---
+++ controller_vae_configs.py
@@ -0,0 +1,278 @@
+# Copyright 2021 The Magenta Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+"""Configurations for ControllerVAE models."""
+import collections
+
+from magenta.common import merge_hparams
+from magenta.contrib import training as contrib_training
+from magenta.models.music_vae import data
+import MusicVAE.lstm_models as lstm_models
+from MusicVAE.base_model import MusicVAE
+from MusicVAE.trained_model import TrainedModel
+import tensorflow.compat.v1 as tf
+import tensorflow_probability as tfp
+
+HParams = contrib_training.HParams
+ds = tfp.distributions
+
+
+class Config(collections.namedtuple(
+    'Config',
+    ['model', 'controller_model', 'hparams', 'note_sequence_augmenter', 'data_converter',
+     'train_examples_path', 'eval_examples_path', 'tfds_name'])):
+
+  def values(self):
+    return self._asdict()
+
+Config.__new__.__defaults__ = (None,) * len(Config._fields)
+
+
+def update_config(config, update_dict):
+  config_dict = config.values()
+  config_dict.update(update_dict)
+  return Config(**config_dict)
+
+
+CONFIG_MAP = {}
+
+
+# class TrainedModelEncoder(base_model.BaseEncoder):
+#   """Encodes given data with pre-trained model."""
+
+#   def __init__(self, original_encoder):
+#     self._original_encoder = original_encoder
+
+#   def build(self, model, hparams, is_training=True):
+#     self._model = model
+
+#   def encode_with_trained_model(self, sequence, sequence_length):
+#     return self._model.encode_tensors(sequence, sequence_length)
+
+
+# class TrainedModelDecoder(base_model.BaseDecoder):
+#   """Decodes latent vectors with pre-trained model."""
+
+#   def __init__(self, original_decoder):
+#     self._original_decoder = original_decoder
+
+#   def build(self, model, hparams, output_depth, is_training=True):
+#     self._model = model
+
+#   def reconstruction_loss(self, x_input, x_target, x_length, z=None,
+#                           c_input=None):
+#     pass
+
+#   def sample(self, n, max_length=None, z=None, c_input=None):
+#     pass
+
+
+class ControllerVAE(MusicVAE):
+  def __init__(self, encoder, decoder):
+    class TrainedModelEncoder(encoder):
+      """Encodes given data with pre-trained model."""
+
+      def build(self, model, hparams, is_training=False):
+        super().build(hparams, is_training)
+        self._model = model
+
+      def encode_with_trained_model(self, sequence, sequence_length):
+        return self._model.encode_tensors(sequence, sequence_length)
+
+    class TrainedModelDecoder(decoder):
+      """Decodes latent vectors with pre-trained model."""
+
+      def build(self, model, hparams, output_depth, is_training=False):
+        super().build(hparams, output_depth, is_training)
+        self._model = model
+
+      def reconstruction_loss(self, x_input, x_target, x_length, z=None,
+                              c_input=None):
+        pass
+
+      def sample(self, n, max_length=None, z=None, c_input=None):
+        pass
+
+    self._encoder = TrainedModelEncoder()
+    self._decoder = TrainedModelDecoder()
+
+
+  def build(self, config, hparams, checkpoint_dir_or_path, output_depth, is_training=False):
+    tf.logging.info('Building MusicVAE model with %s, %s, and hparams:\n%s',
+                    self.encoder.__class__.__name__,
+                    self.decoder.__class__.__name__, hparams.values())
+    self.global_step = tf.train.get_or_create_global_step()
+    self._hparams = hparams
+
+    trained_model = TrainedModel(
+      config, batch_size=hparams.batch_size,
+      checkpoint_dir_or_path=checkpoint_dir_or_path)
+
+    self._encoder.build(trained_model, hparams, is_training)
+    self._decoder.build(trained_model, hparams, output_depth, is_training)
+
+
+  def encode_controller_vae(self, sequence, sequence_length, control_sequence=None):
+    hparams = self.hparams
+    controller_z_size = hparams.controller_z_size
+    intermediate_size = hparams.intermediate_size
+
+    sequence = tf.to_float(sequence)
+    if control_sequence is not None:
+      control_sequence = tf.to_float(control_sequence)
+      sequence = tf.concat([sequence, control_sequence], axis=-1)
+
+    encoder_output, _, _ = self.encoder.encode_with_trained_model(sequence, sequence_length)
+
+    for layer, size in enumerate(intermediate_size, 1):
+      encoder_output = tf.layers.dense(
+        encoder_output,
+        size,
+        activation=tf.nn.leaky_relu,
+        name='encoder/intermediate_%i' % layer,
+        kernel_initializer=tf.keras.initializers.glorot_normal)
+
+    mu = tf.layers.dense(
+        encoder_output,
+        controller_z_size,
+        name='encoder/mu',
+        kernel_initializer=tf.keras.initializers.glorot_normal)
+    sigma = tf.layers.dense(
+        encoder_output,
+        controller_z_size,
+        activation=tf.nn.softplus,
+        name='encoder/sigma',
+        kernel_initializer=tf.keras.initializers.glorot_normal)
+
+    return ds.MultivariateNormalDiag(loc=mu, scale_diag=sigma)
+
+
+  def decode_controller_vae(self, z):
+    hparams = self.hparams
+    z_size = hparams.z_size
+    intermediate_size = reversed(hparams.intermediate_size)
+    decoder_output = z
+
+    for layer, size in enumerate(intermediate_size, 1):
+      decoder_output = tf.layers.dense(
+        decoder_output,
+        size,
+        activation=tf.nn.leaky_relu,
+        name='decoder/intermediate_%i' % layer,
+        kernel_initializer=tf.keras.initializers.glorot_normal)
+
+    decoder_output = tf.layers.dense(
+      decoder_output,
+      z_size,
+      activation=tf.nn.leaky_relu,
+      name='decoder/output',
+      kernel_initializer=tf.keras.initializers.glorot_normal)
+
+
+  def _compute_model_loss(
+      self, input_sequence, output_sequence, sequence_length, control_sequence):
+    hparams = self.hparams
+    batch_size = hparams.batch_size
+
+    input_sequence = tf.to_float(input_sequence)
+    output_sequence = tf.to_float(output_sequence)
+
+    max_seq_len = tf.minimum(tf.shape(output_sequence)[1], hparams.max_seq_len)
+
+    input_sequence = input_sequence[:, :max_seq_len]
+
+    if control_sequence is not None:
+      control_depth = control_sequence.shape[-1]
+      control_sequence = tf.to_float(control_sequence)
+      control_sequence = control_sequence[:, :max_seq_len]
+      # Shouldn't be necessary, but the slice loses shape information when
+      # control depth is zero.
+      control_sequence.set_shape([batch_size, None, control_depth])
+
+    # The target/expected outputs.
+    x_target = output_sequence[:, :max_seq_len]
+    # Inputs to be fed to decoder, including zero padding for the initial input.
+    x_input = tf.pad(output_sequence[:, :max_seq_len - 1],
+                     [(0, 0), (1, 0), (0, 0)])
+    x_length = tf.minimum(sequence_length, max_seq_len)
+
+    # Either encode to get `z`, or do unconditional, decoder-only.
+    if hparams.z_size:  # vae mode:
+      q_z = self.encode_controller_vae(input_sequence, x_length, control_sequence)
+      z = q_z.sample()
+      z = self.decode_controller_vae(z)
+
+      # Prior distribution.
+      p_z = ds.MultivariateNormalDiag(
+          loc=[0.] * hparams.z_size, scale_diag=[1.] * hparams.z_size)
+
+      # KL Divergence (nats)
+      kl_div = ds.kl_divergence(q_z, p_z)
+
+      # Concatenate the Z vectors to the inputs at each time step.
+    else:  # unconditional, decoder-only generation
+      kl_div = tf.zeros([batch_size, 1], dtype=tf.float32)
+      z = None
+
+    r_loss, metric_map = self.decoder.reconstruction_loss(
+        x_input, x_target, x_length, z, control_sequence)[0:2]
+
+    free_nats = hparams.free_bits * tf.math.log(2.0)
+    kl_cost = tf.maximum(kl_div - free_nats, 0)
+
+    beta = ((1.0 - tf.pow(hparams.beta_rate, tf.to_float(self.global_step)))
+            * hparams.max_beta)
+    self.loss = tf.reduce_mean(r_loss) + beta * tf.reduce_mean(kl_cost)
+
+    scalars_to_summarize = {
+        'loss': self.loss,
+        'losses/r_loss': r_loss,
+        'losses/kl_loss': kl_cost,
+        'losses/kl_bits': kl_div / tf.math.log(2.0),
+        'losses/kl_beta': beta,
+    }
+    return metric_map, scalars_to_summarize
+
+
+
+CONFIG_MAP['cat-drums_2bar_small_3dim'] = Config(
+    model=MusicVAE(lstm_models.BidirectionalLstmEncoder(),
+                   lstm_models.CategoricalLstmDecoder()),
+    controller_model=ControllerVAE(lstm_models.BidirectionalLstmEncoder,
+                        lstm_models.CategoricalLstmDecoder),
+    hparams=merge_hparams(
+        lstm_models.get_default_hparams(),
+        HParams(
+            batch_size=512,
+            max_seq_len=32,  # 2 bars w/ 16 steps per bar
+            z_size=256,
+            intermediate_size=[128],
+            controller_z_size=3,
+            enc_rnn_size=[512],
+            dec_rnn_size=[256, 256],
+            free_bits=48,
+            max_beta=0.2,
+            sampling_schedule='inverse_sigmoid',
+            sampling_rate=1000,
+        )),
+    note_sequence_augmenter=None,
+    data_converter=data.DrumsConverter(
+        max_bars=100,  # Truncate long drum sequences before slicing.
+        slice_bars=2,
+        steps_per_quarter=4,
+        roll_input=True),
+    train_examples_path=None,
+    eval_examples_path=None,
+)
--- controller_vae_train.py
+++ controller_vae_train.py
@@ -22,12 +22,12 @@ import os
 import glob
 
 import MusicVAE.configs as configs
+from magenta.models.music_vae import data
 from MusicVAE.trained_model import TrainedModel
 import note_seq
 import numpy as np
 import tensorflow.compat.v1 as tf
-
-import CompressionVAE.cvae.cvae as cvae
+import tf_slim
 
 flags = tf.app.flags
 logging = tf.logging
@@ -46,6 +46,9 @@ flags.DEFINE_integer(
     'num_outputs', 5,
     'In `sample` mode, the number of samples to produce. In `interpolate` '
     'mode, the number of steps (including the endpoints).')
+flags.DEFINE_string(
+    'master', '',
+    'The TensorFlow master to use.')
 flags.DEFINE_string(
     'examples_path', None,
     'Path to a TFRecord file of NoteSequence examples. Overrides the config.')
@@ -74,15 +77,238 @@ flags.DEFINE_string(
     'tfds_name', None,
     'TensorFlow Datasets dataset name to use. Overrides the config.')
 flags.DEFINE_string(
-    'logdir', 'temp',
-    'Location for where to save the model and other related files.')
+    'run_dir', None,
+    'Path where checkpoints and summary events will be located during '
+    'training and evaluation. Separate subdirectories `train` and `eval` '
+    'will be created within this directory.')
 flags.DEFINE_integer(
     'num_steps', 200000,
     'Number of training steps or `None` for infinite.')
+flags.DEFINE_integer(
+    'eval_num_batches', None,
+    'Number of batches to use during evaluation or `None` for all batches '
+    'in the data source.')
+flags.DEFINE_integer(
+    'checkpoints_to_keep', 100,
+    'Maximum number of checkpoints to keep in `train` mode or 0 for infinite.')
+flags.DEFINE_integer(
+    'keep_checkpoint_every_n_hours', 1,
+    'In addition to checkpoints_to_keep, keep a checkpoint every N hours.')
+flags.DEFINE_string(
+    'mode', 'train',
+    'Which mode to use (`train` or `eval`).')
+flags.DEFINE_string(
+    'hparams', '',
+    'A comma-separated list of `name=value` hyperparameter values to merge '
+    'with those in the config.')
+flags.DEFINE_bool(
+    'cache_dataset', True,
+    'Whether to cache the dataset in memory for improved training speed. May '
+    'cause memory errors for very large datasets.')
+flags.DEFINE_integer(
+    'task', 0,
+    'The task number for this worker.')
+flags.DEFINE_integer(
+    'num_ps_tasks', 0,
+    'The number of parameter server tasks.')
+flags.DEFINE_integer(
+    'num_sync_workers', 0,
+    'The number of synchronized workers.')
+flags.DEFINE_string(
+    'eval_dir_suffix', '',
+    'Suffix to add to eval output directory.')
 flags.DEFINE_string(
     'log', 'INFO',
     'The threshold for what messages will be logged: '
     'DEBUG, INFO, WARN, ERROR, or FATAL.')
+
+
+# Should not be called from within the graph to avoid redundant summaries.
+def _trial_summary(hparams, examples_path, output_dir):
+  """Writes a tensorboard text summary of the trial."""
+
+  examples_path_summary = tf.summary.text(
+      'examples_path', tf.constant(examples_path, name='examples_path'),
+      collections=[])
+
+  hparams_dict = hparams.values()
+
+  # Create a markdown table from hparams.
+  header = '| Key | Value |\n| :--- | :--- |\n'
+  keys = sorted(hparams_dict.keys())
+  lines = ['| %s | %s |' % (key, str(hparams_dict[key])) for key in keys]
+  hparams_table = header + '\n'.join(lines) + '\n'
+
+  hparam_summary = tf.summary.text(
+      'hparams', tf.constant(hparams_table, name='hparams'), collections=[])
+
+  with tf.Session() as sess:
+    writer = tf.summary.FileWriter(output_dir, graph=sess.graph)
+    writer.add_summary(examples_path_summary.eval())
+    writer.add_summary(hparam_summary.eval())
+    writer.close()
+
+
+def _get_input_tensors(dataset, config):
+  """Get input tensors from dataset."""
+  batch_size = config.hparams.batch_size
+  iterator = tf.data.make_one_shot_iterator(dataset)
+  (input_sequence, output_sequence, control_sequence,
+   sequence_length) = iterator.get_next()
+  input_sequence.set_shape(
+      [batch_size, None, config.data_converter.input_depth])
+  output_sequence.set_shape(
+      [batch_size, None, config.data_converter.output_depth])
+  if not config.data_converter.control_depth:
+    control_sequence = None
+  else:
+    control_sequence.set_shape(
+        [batch_size, None, config.data_converter.control_depth])
+  sequence_length.set_shape([batch_size] + sequence_length.shape[1:].as_list())
+
+  return {
+      'input_sequence': input_sequence,
+      'output_sequence': output_sequence,
+      'control_sequence': control_sequence,
+      'sequence_length': sequence_length
+  }
+
+
+def train_controller_vae(train_dir,
+          config,
+          dataset_fn,
+          checkpoints_to_keep=5,
+          keep_checkpoint_every_n_hours=1,
+          num_steps=None,
+          master='',
+          num_sync_workers=0,
+          num_ps_tasks=0,
+          task=0):
+  """Train loop."""
+  tf.gfile.MakeDirs(train_dir)
+  is_chief = (task == 0)
+  if is_chief:
+    _trial_summary(
+        config.hparams, config.train_examples_path or config.tfds_name,
+        train_dir)
+  with tf.Graph().as_default():
+    with tf.device(tf.train.replica_device_setter(
+        num_ps_tasks, merge_devices=True)):
+
+      model = config.model
+      model.build(config.hparams,
+                  config.data_converter.output_depth)
+
+      optimizer = model.train(**_get_input_tensors(dataset_fn(), config))
+
+      hooks = []
+      if num_sync_workers:
+        optimizer = tf.train.SyncReplicasOptimizer(
+            optimizer,
+            num_sync_workers)
+        hooks.append(optimizer.make_session_run_hook(is_chief))
+
+      grads, var_list = list(zip(*optimizer.compute_gradients(model.loss)))
+      global_norm = tf.global_norm(grads)
+      tf.summary.scalar('global_norm', global_norm)
+
+      if config.hparams.clip_mode == 'value':
+        g = config.hparams.grad_clip
+        clipped_grads = [tf.clip_by_value(grad, -g, g) for grad in grads]
+      elif config.hparams.clip_mode == 'global_norm':
+        clipped_grads = tf.cond(
+            global_norm < config.hparams.grad_norm_clip_to_zero,
+            lambda: tf.clip_by_global_norm(  # pylint:disable=g-long-lambda
+                grads, config.hparams.grad_clip, use_norm=global_norm)[0],
+            lambda: [tf.zeros(tf.shape(g)) for g in grads])
+      else:
+        raise ValueError(
+            'Unknown clip_mode: {}'.format(config.hparams.clip_mode))
+      train_op = optimizer.apply_gradients(
+          list(zip(clipped_grads, var_list)),
+          global_step=model.global_step,
+          name='train_step')
+
+      logging_dict = {'global_step': model.global_step,
+                      'loss': model.loss}
+
+      hooks.append(tf.train.LoggingTensorHook(logging_dict, every_n_iter=100))
+      if num_steps:
+        hooks.append(tf.train.StopAtStepHook(last_step=num_steps))
+
+      scaffold = tf.train.Scaffold(
+          saver=tf.train.Saver(
+              max_to_keep=checkpoints_to_keep,
+              keep_checkpoint_every_n_hours=keep_checkpoint_every_n_hours))
+      tf_slim.training.train(
+          train_op=train_op,
+          logdir=train_dir,
+          scaffold=scaffold,
+          hooks=hooks,
+          save_checkpoint_secs=60,
+          master=master,
+          is_chief=is_chief)
+
+
+def evaluate(train_dir,
+             eval_dir,
+             config,
+             dataset_fn,
+             num_batches,
+             master=''):
+  """Evaluate the model repeatedly."""
+  tf.gfile.MakeDirs(eval_dir)
+
+  _trial_summary(
+      config.hparams, config.eval_examples_path or config.tfds_name, eval_dir)
+  with tf.Graph().as_default():
+    model = config.model
+    model.build(config.hparams,
+                config.data_converter.output_depth)
+
+    eval_op = model.eval(
+        **_get_input_tensors(dataset_fn().take(num_batches), config))
+
+    hooks = [
+        tf_slim.evaluation.StopAfterNEvalsHook(num_batches),
+        tf_slim.evaluation.SummaryAtEndHook(eval_dir)
+    ]
+    tf_slim.evaluation.evaluate_repeatedly(
+        train_dir,
+        eval_ops=eval_op,
+        hooks=hooks,
+        eval_interval_secs=60,
+        master=master)
+
+
+def get_input_note_sequences(config):
+  examples_path = config.train_examples_path + '/*.mid'
+  midi_paths = glob.glob(examples_path)
+  
+  def _check_extract_examples(input_ns, path):
+    """Make sure each input returns exactly one example from the converter."""
+    tensors = config.data_converter.to_tensors(input_ns).outputs
+    if not tensors or len(tensors) > 1:
+      print(
+          'MusicVAE configs have very specific input requirements. Could not '
+          'extract any valid inputs from `%s`.' % path)
+      isValid = False
+      return isValid
+    else:
+      isValid = True
+      return isValid
+
+  note_sequences = []
+  for _, input_path in enumerate(midi_paths):
+    input_midi = os.path.expanduser(input_path)
+    input = note_seq.midi_file_to_note_sequence(input_midi)
+    if _check_extract_examples(input, input_path):
+      note_sequences.append(input)
+    else:
+      continue
+  print('Number of Valid NoteSequences: %i' % len(note_sequences))
+
+  return note_sequences
 
 
 def encode_dataset(
@@ -157,23 +383,33 @@ def load_model(config_map):
   logging.info('Loading model...')
   checkpoint_dir_or_path = os.path.expanduser(FLAGS.checkpoint_file)
   model = TrainedModel(
-      config, batch_size=min(FLAGS.max_batch_size, FLAGS.num_outputs),
+      config, batch_size=config.hparams.batch_size,
       checkpoint_dir_or_path=checkpoint_dir_or_path)
 
   return model
 
 
 def train(config_map,
-        model):
+        model,
+        tf_file_reader=tf.data.TFRecordDataset,
+        file_reader=tf.python_io.tf_record_iterator):
 
-  logdir = os.path.expanduser(FLAGS.logdir)
+  if not FLAGS.run_dir:
+    raise ValueError('Invalid run directory: %s' % FLAGS.run_dir)
+  run_dir = os.path.expanduser(FLAGS.run_dir)
+  train_dir = os.path.join(run_dir, 'train')
+
+  if FLAGS.mode not in ['train', 'eval']:
+    raise ValueError('Invalid mode: %s' % FLAGS.mode)
 
   if FLAGS.config not in config_map:
     raise ValueError('Invalid config: %s' % FLAGS.config)
   config = config_map[FLAGS.config]
+  if FLAGS.hparams:
+    config.hparams.parse(FLAGS.hparams)
   config_update_map = {}
   if FLAGS.examples_path:
-    config_update_map['train_examples_path'] = os.path.expanduser(
+    config_update_map['%s_examples_path' % FLAGS.mode] = os.path.expanduser(
         FLAGS.examples_path)
   if FLAGS.tfds_name:
     if FLAGS.examples_path:
@@ -183,51 +419,78 @@ def train(config_map,
     config_update_map['eval_examples_path'] = None
     config_update_map['train_examples_path'] = None
   config = configs.update_config(config, config_update_map)
+  if FLAGS.num_sync_workers:
+    config.hparams.batch_size //= FLAGS.num_sync_workers
 
-  z = encode_dataset(
-      model,
-      config)
+  if FLAGS.mode == 'train':
+    is_training = True
+  elif FLAGS.mode == 'eval':
+    is_training = False
+  else:
+    raise ValueError('Invalid mode: {}'.format(FLAGS.mode))
+
+  def dataset_fn():
+    return data.get_dataset(
+        config,
+        tf_file_reader=tf_file_reader,
+        is_training=is_training,
+        cache_dataset=FLAGS.cache_dataset)
+
+  if is_training:
+    train_controller_vae(
+        train_dir,
+        config=config,
+        dataset_fn=dataset_fn,
+        checkpoints_to_keep=FLAGS.checkpoints_to_keep,
+        keep_checkpoint_every_n_hours=FLAGS.keep_checkpoint_every_n_hours,
+        num_steps=FLAGS.num_steps,
+        master=FLAGS.master,
+        num_sync_workers=FLAGS.num_sync_workers,
+        num_ps_tasks=FLAGS.num_ps_tasks,
+        task=FLAGS.task)
+  else:
+    num_batches = FLAGS.eval_num_batches or data.count_examples(
+        config.eval_examples_path,
+        config.tfds_name,
+        config.data_converter,
+        file_reader) // config.hparams.batch_size
+    eval_dir = os.path.join(run_dir, 'eval' + FLAGS.eval_dir_suffix)
+    evaluate(
+        train_dir,
+        eval_dir,
+        config=config,
+        dataset_fn=dataset_fn,
+        num_batches=num_batches,
+        master=FLAGS.master)
+
+
+# def embed_decode(cvae_model, z):
+#   print(z)
+#   zz = cvae_model.embed(z)
+#   print(zz)
+#   _z = cvae_model.decode(zz)
+#   print(_z)
+
+
+# def create_train_data(data_size=256, data_num=10000):
+#   train_data = np.random.randn(data_num, data_size).astype(np.float32)
+#   return train_data
+
+
+# def train_with_random_vectors(train_data):
+#   logdir = os.path.expanduser(FLAGS.logdir)
+
+#   cvae_model = cvae.CompressionVAE(
+#                 train_data,
+#                 dim_latent=3,
+#                 iaf_flow_length=5,
+#                 batch_size=128,
+#                 batch_size_test=128,
+#                 logdir=logdir)
   
-  cvae_model = cvae.CompressionVAE(
-                z,
-                dim_latent=3,
-                iaf_flow_length=5,
-                batch_size=config.hparams.batch_size,
-                batch_size_test=config.hparams.batch_size,
-                logdir=logdir)
-  
-  cvae_model.train()
+#   cvae_model.train()
 
-  return cvae_model, z
-
-
-def embed_decode(cvae_model, z):
-  print(z)
-  zz = cvae_model.embed(z)
-  print(zz)
-  _z = cvae_model.decode(zz)
-  print(_z)
-
-
-def create_train_data(data_size=256, data_num=10000):
-  train_data = np.random.randn(data_num, data_size).astype(np.float32)
-  return train_data
-
-
-def train_with_random_vectors(train_data):
-  logdir = os.path.expanduser(FLAGS.logdir)
-
-  cvae_model = cvae.CompressionVAE(
-                train_data,
-                dim_latent=3,
-                iaf_flow_length=5,
-                batch_size=128,
-                batch_size_test=128,
-                logdir=logdir)
-  
-  cvae_model.train()
-
-  return cvae_model
+#   return cvae_model
 
 
 def run(config_map):

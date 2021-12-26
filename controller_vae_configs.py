8b02e041e4bd21c8ae7306105d250bd3213d09e6
ryorod <ryo3ishi28@gmail.com>
2021-12-26 18:05:41 +0900

    update

--- controller_vae_configs.py
+++ controller_vae_configs.py
@@ -121,3 +121,29 @@ CONFIG_MAP['hierdec-trio_16bar_3dim'] = Config(
     train_examples_path=None,
     eval_examples_path=None,
 )
+
+CONFIG_MAP['bass_16bar_3dim'] = Config(
+    model=ControllerVAE(
+        lstm_models.BidirectionalLstmEncoder(),
+        lstm_models.HierarchicalLstmDecoder(
+            lstm_models.CategoricalLstmDecoder(),
+            level_lengths=[16, 16],
+            disable_autoregression=True)),
+    hparams=merge_hparams(
+        lstm_models.get_default_hparams(),
+        HParams(
+            batch_size=256,
+            max_seq_len=256,
+            original_z_size=512,
+            intermediate_size=[256, 128],
+            z_size=3,
+            enc_rnn_size=[2048, 2048],
+            dec_rnn_size=[1024, 1024],
+            free_bits=256,
+            max_beta=0.2,
+        )),
+    note_sequence_augmenter=None,
+    data_converter=trio_16bar_converter,
+    train_examples_path=None,
+    eval_examples_path=None,
+)
